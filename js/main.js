3eadf21026d8eac671d2a8623cf95b92f24d2749
sobi <95006149+hellariusobi@users.noreply.github.com>
2024-10-04 16:04:24 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,3 +5,20 @@ menu.onclick = () => {
   menu.classList.toggle("bx-x");
   navlist.classList.toggle("open");
 };
+function myFunction() {
+  document.getElementById("mydropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-menu");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
