6e7669dc46e4713383789806bc54e15a2237375f
sobi <95006149+hellariusobi@users.noreply.github.com>
2024-11-18 13:37:59 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,24 +1,33 @@
-let menu = document.querySelector("#menu-icon");
-let navlist = document.querySelector("#navigation");
-
-menu.onclick = () => {
-  menu.classList.toggle("bx-x");
-  navlist.classList.toggle("open");
-};
 function myFunction() {
-  document.getElementById("mydropdown").classList.toggle("show");
+  document.getElementById("mydropdown-content").classList.toggle("show");
 }
-
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function (event) {
-  if (!event.target.matches(".dropbtn")) {
-    var dropdowns = document.getElementsByClassName("dropdown-menu");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains("show")) {
-        openDropdown.classList.remove("show");
-      }
+window.onclick = function (e) {
+  if (!e.target.matches(".dropbtn")) {
+    var myDropdown = document.getElementById("mydropdown-content");
+    if (myDropdown.classList.contains("show")) {
+      myDropdown.classList.remove("show");
     }
   }
+};
+
+var dropdown2 = document.getElementsByClassName("dropbtn2");
+var i;
+
+for (i = 0; i < dropdown2.length; i++) {
+  dropdown2[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
+let sidebar = document.querySelector("#menu-icon");
+let sidebarmenu = document.querySelector("#sidebarmobile");
+
+sidebar.onclick = () => {
+  sidebar.classList.toggle("bx-x");
+  sidebarmenu.classList.toggle("open");
 };
